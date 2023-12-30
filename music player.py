97a322b44b2085d5e3d9b46a0a4861d8e607065c
지하 <jihyun.0528.9123@gmail.com>
2023-12-30 20:27:50 +0900

    git 함

---
+++ music player.py
@@ -0,0 +1,49 @@
+#mp3 재생 프로그램
+from tkinter import *
+from tkinter import filedialog
+from pygame import *
+import os
+root= Tk()
+root.title("mp3 재생 프로그램")
+root.geometry("500x600+500+40")
+root.resizable(False,False)
+from playsound import playsound
+
+
+filename=0
+clicked=0
+    
+img1=PhotoImage(file="click.png")
+label_img=Label(root, image=img1)
+label_img.pack()
+
+img2=PhotoImage(file="play.png")
+label_img=Label(root, image=img2)
+label_img.pack()
+label_img.bind("<Button-1>", lambda e: play_sound())
+
+img3=PhotoImage(file="stop.png")
+label_img=Label(root, image=img3)
+label_img.pack()
+
+def openfile():
+    global filename
+    filename = filedialog.askopenfilename(
+        initialdir="C:/Users/Public",
+        title="open file", 
+        filetypes=(("mp3 files", "*.mp3"), ("all files", "*.*"))
+    )
+
+def play_sound():
+    playsound("Photograph.mp3")
+
+
+    
+def click_img1(event):
+    openfile()
+
+
+
+# 윈도우에 마우스 클릭 이벤트 핸들러 등록
+root.bind("<Button-2>", click_img1)  # <Button-1>은 왼쪽 버튼을 의미합니다
+root.mainloop()
